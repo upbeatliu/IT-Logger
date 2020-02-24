6d026356f1ce757154f5969798e09f7fd330ded5
Sunny Pig <upbeatliu@gmail.com>
2020-02-25 09:37:21 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js'
+import './App.css';
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
+
+const App = () => {
+  useEffect(() => {
+    //init Materialize JS
+    M.AutoInit();
+  });
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
