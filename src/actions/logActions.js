efe12f4b8a2cfbd9054293bc82907905dd2cd628
Sunny Pig <upbeatliu@gmail.com>
2020-02-28 12:18:44 +1000

    fixed bug, add log and delete log from backend

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types'
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types'
 
 //get logs from server
 export const getLogs = () => async dispatch => {
@@ -8,7 +8,51 @@ export const getLogs = () => async dispatch => {
     const data = await res.json();
     dispatch({
       type: GET_LOGS,
-      logs: data
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+}
+
+// Add log
+export const addLog = (log) => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+}
+
+//delete log from server
+export const deleteLog = (id) => async dispatch => {
+  try {
+    setLoading();
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     });
   } catch (err) {
     dispatch({
--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { addLog } from '../../actions/logActions'
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-export const AddLogModal = () => {
+export const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
-      M.toast({ html: 'Please enter a message and tech' })
+      M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log('Message:' + message, ' Tech' + tech, ' Attention:' + attention);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date()
+      }
+      addLog(newLog);
+      M.toast({ html: `Log added by ${tech}` });
+
       //clear all fields
       setMessage('');
       setTech('');
@@ -60,5 +71,8 @@ const modalStyle = {
   width: '75%',
   hight: '75%'
 }
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired,
+}
 
-export default AddLogModal
+export default connect(null, { addLog })(AddLogModal)
